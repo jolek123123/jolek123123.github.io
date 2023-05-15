66d94e3212dc6c77fe3671510c728a7c61eed614
jolek123123 <121031390+jolek123123@users.noreply.github.com>
2023-05-15 13:59:15 +0200

    Add files via upload

--- kartka.js
+++ kartka.js
@@ -4,14 +4,34 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 kaboom();
 
 loadSprite('kartka','kartka.png')
+loadSprite('balwan','balwan.png')
+loadSound('muzyka','do_szopy_hej_pasterze.mp3')
+
 
 add([
     sprite('kartka')
 ])
 
-loadSprite('balwan','balwan.png')
 
-add([
+
+const balwan = add([
     sprite('balwan'),
     pos(200,200)
 ])
+
+let wprawo = true
+
+
+balwan.onUpdate(()=> {
+    if(wprawo && balwan.pos.x < 600)  
+    balwan.pos.x += 1
+    else if (!wprawo && balwan.pos.x > 50)
+    balwan.pos.x -=1
+    else wprawo ^= true
+})
+
+
+
+ onMouseRelease(()=> play("muzyka"))
+
+
